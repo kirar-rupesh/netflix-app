d581380b003eb9a86c7a1aedf62ce0bc3340fa24
kirar-rupesh <rupeshkirar250895@gmail.com>
2023-03-25 01:38:26 +0530

    adding files again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Routers, Routes, Route} from 'react-router-dom';
+import Home from './Components/Home';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routers>
+      <Header/>
+      <Routes>
+       <Route path="/"  element={<Home />} />
+      </Routes>
+    </Routers>
+      
     </div>
   );
 }
